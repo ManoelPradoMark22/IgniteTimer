428a6108c296bb229d9a314e390ab5ef4ad31262
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-07 16:53:06 -0300

    setting ESlint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -13,6 +13,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 0.5rem;
 
-  background-color: ${props => props.theme['green-500']};
-  color: ${props => props.theme.white}
-`;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+`
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,4 @@
-import { ButtonContainer } from "./Button.styles";
-import { ButtonVariant } from './Button.styles'
+import { ButtonContainer, ButtonVariant } from './Button.styles'
 
 interface ButtonProps {
   variant?: ButtonVariant
