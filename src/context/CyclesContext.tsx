0e187cdcd411631b5d3d084c6ad5e9bb25b13c4a
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-22 21:17:59 -0300

    mark cycle as finished with reducer

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -42,25 +42,31 @@ export function CyclesContextProvider({
 }: CyclesContextProviderProps) {
   const [cyclesState, dispatch] = useReducer(
     (state: CyclesState, action) => {
-      if (action.type === 'ADD_NEW_CYCLE') {
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id,
-        }
-      } else if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            return cycle.id === state.activeCycleId
-              ? { ...cycle, interruptedDate: new Date() }
-              : cycle
-          }),
-          activeCycleId: null,
-        }
+      const actionTypeKey =
+        action.type === 'INTERRUPT_CURRENT_CYCLE'
+          ? 'interruptedDate'
+          : 'finishedDate'
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          }
+        case 'INTERRUPT_CURRENT_CYCLE':
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              return cycle.id === state.activeCycleId
+                ? { ...cycle, [actionTypeKey]: new Date() }
+                : cycle
+            }),
+            activeCycleId: null,
+          }
+        default:
+          return state
       }
-
-      return state
     },
     {
       cycles: [],
